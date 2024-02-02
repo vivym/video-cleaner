05f083fa3203281b8358c4324b6f2ac4a15a8ac7
Ming Yang <vivym@live.com>
2024-02-02 14:32:33 +0800

    sync
    Signed-off-by: Ming Yang <vivym@live.com>

--- video_cleaner/pipelines/pipeline_propainter.py
+++ video_cleaner/pipelines/pipeline_propainter.py
@@ -1,14 +1,17 @@
 from video_cleaner.models import RAFT, RecurrentFlowCompleteNet, InpaintGenerator
+from .pipeline_raft import RAFTPipeline
 
 
 class PropainterPipeline:
     def __init__(
         self,
-        raft: RAFT,
+        raft_pipe: RAFTPipeline,
         flow_complete_net: RecurrentFlowCompleteNet,
         inpaint_generator: InpaintGenerator,
     ):
-        ...
+        self.raft_pipe = raft_pipe
+        self.flow_complete_net = flow_complete_net
+        self.inpaint_generator = inpaint_generator
 
     def __call__(self):
         ...
